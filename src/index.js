68c8e86323b61aafd9759b1f0bd992d19133697f
rutu <rutuparna1810@gmail.com>
2021-05-24 12:06:50 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const SauravsFunction = () =>{
+    console.log("Created by Saurav");
+ 
+}
+
+const RutuFunction = () =>{
     console.log("Created by Rutu");
     console.log("Change done");
 }
-
 SauravsFunction()
+RutuFunction()
