06de1ae235bb8ab59f49631d294645e6826113ce
Saurav <sauravtewatia93@gmail.com>
2021-05-24 12:31:39 +0530

    another commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 const SauravsFunction = () => {
-<<<<<<< HEAD
   console.log("Created by Saurav");
   console.log("added some more code");
 };
 
 SauravsFunction();
-=======
-  console.log('Created by Saurav');
-};
 
-<<<<<<< HEAD
-SauravsFunction()
+console.log("This is done by Mounika");
 
-console.log("This is done by Mounika")
-=======
 const ManjunathFunction = () => {
-  console.log('Created by Manjunath🙂');
+  console.log("Created by Manjunath🙂");
 };
 
-SauravsFunction();
 ManjunathFunction();
->>>>>>> 966cd44b4cef7cdb3e6c362aab7f0529cc9b2099
